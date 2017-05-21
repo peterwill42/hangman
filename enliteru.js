e0a81b272e55b7459e8be367099c466d839bdce7
Joanne Joanne <navear@gmail.com>
2017-05-21 15:25:47 +1000

    reprovanta

--- enliteru.js
+++ enliteru.js
@@ -2,22 +2,23 @@ var ltr = "" ;
 var amasigu = [];
 
 function litero(elekto) {
-  document.getElementById("montru").innerHTML = elekto + "but";
+
   montruLiteron(elekto);
    Letter(elekto);
  
-   document.getElementById(elekto+"but").style.visiblity = "hidden";
+
  };
 
 function montruLiteron(ltr)
 {
+  document.getElementById(ltr+"but").style.visiblity = "hidden";
   //here are the used letters
   amasigu = amasigu + ltr;
   //limits number of key entries to avoid overflow
   if (amasigu.length >= 28)
   {
     amasigu = [];
-    document.getElementById("malkaŝu").innerHTML = "sufiĉas !"
+    document.getElementById("malkaŝu").innerHTML = "sufiĉas !";
   }
   document.getElementById("amasigu").innerHTML=amasigu.toUpperCase();
 };
