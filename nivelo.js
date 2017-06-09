9069d9d7f303ce8679df021161ba415e8f4c8f5b
peterwill42 <athodyd@optusnet.com.au>
2017-06-09 20:24:55 +1000

    Add files via upload
    gxisdatigi. venko kaj malvenko mesaĝoj
    vortListo

--- nivelo.js
+++ nivelo.js
@@ -4,4 +4,9 @@ function niveloj()
 {
   selx = parseInt(document.getElementById("selekto").value);
   document.getElementById("provado").innerHTML = selx;
+
+
+//store and retrieve selx (preserve setting)
+//window.sessionStorage.setItem("konservu", selx);
+//var konservu = parseInt(window.sessionStorage.getItem("konservu"));
 };
--- takso.js
+++ takso.js
@@ -2,6 +2,8 @@ var poentoj = 0;
 var vicoj = 0;
 var vera = 0;
 var ĝusta = 0;
+var korekta = 0;
+
 
 //show countdown starting point for the new game
 var dekalkulo = selx;
@@ -22,6 +24,13 @@ function skribo(elekto)
         if (ĝusta>0)
         {
           ĝusta = 1;
+          //count correct letters
+          korekta = korekta + ĝusta;
+          //if all letters correct then win
+          if (korekta>=hazardaVorto.length)
+          {
+          document.getElementById("venkas").innerHTML = "VI VENKAS";
+        }
         }
       }
     };
@@ -31,13 +40,14 @@ vera=vera+ĝusta;
 ĝusta=0;
 vicoj++;
 poentoj = vicoj-vera;
-//set countdown starting number here
+//set countdown starting number
 dekalkulo = selx-poentoj;
 // if zero reveal answer
 if (dekalkulo <1)
 {
   dekalkulo = 0;
   malkaŝu();
+  document.getElementById("venkas").innerHTML = "pli bonan ŝancon venontfoje";
 }
 document.getElementById("provado").innerHTML = dekalkulo;
 document.getElementById("respondo").innerHTML = respondaro.join(" ");
