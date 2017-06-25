c81b09d7ea0a13cb80779b6776482221482557c3
peterwill42 <athodyd@optusnet.com.au>
2017-06-25 16:45:35 +1000

    Create Ludo.js

---
+++ Ludo.js
@@ -0,0 +1,9 @@
+var game=
+{
+//choose a random word from the vortListo array
+hazardaVorto: vortListo[Math.floor(Math.random() * vortListo.length)],
+//global variable
+cheno :"",
+//empty array to store guesses
+respondaro : []
+}
