da182baf7d8743dbb765d8628d11aba750aa381a
peterwill42 <athodyd@optusnet.com.au>
2017-06-25 17:18:41 +1000

    Update Ludo.js

--- Ludo.js
+++ Ludo.js
@@ -5,5 +5,15 @@ hazardaVorto: vortListo[Math.floor(Math.random() * vortListo.length)],
 //global variable
 cheno :"",
 //empty array to store guesses
-respondaro : []
+respondaro : [],
+  
+//takso
+  
+  poentoj : 0,
+  vicoj : 0,
+  vera : 0,
+  ĝusta : 0,
+  korekta : 0,
+  //show countdown starting point for the new game
+  dekalkulo : selx
 }
