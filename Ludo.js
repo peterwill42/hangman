a71ffd5d6fc935407c17a2b40966fecbd367bd9b
peterwill42 <athodyd@optusnet.com.au>
2017-07-23 16:27:26 +1000

    Jo (#4)
    * added function
    
    * remove global variables
    
    * make the new game button work properly

--- Ludo.js
+++ Ludo.js
@@ -1,19 +1,33 @@
-var game=
-{
-//choose a random word from the vortListo array
-hazardaVorto: vortListo[Math.floor(Math.random() * vortListo.length)],
-//global variable
-cheno :"",
-//empty array to store guesses
-respondaro : [],
-  
-//takso
-  
-  poentoj : 0,
-  vicoj : 0,
-  vera : 0,
-  ĝusta : 0,
-  korekta : 0,
-  //show countdown starting point for the new game
-  dekalkulo : selx
+function newgame(){
+	var ludo={
+		//choose a random word from the vortListo array
+		hazardaVorto: vortListo[Math.floor(Math.random() * vortListo.length)],
+		//global variable
+		cheno :"",
+		//empty array to store guesses
+		respondaro : [],
+		  
+		//takso
+	  
+		poentoj : 0,
+		vicoj : 0,
+		vera : 0,
+		ĝusta : 0,
+		korekta : 0,
+		//show countdown starting point for the new game
+		dekalkulo : selx
+	}
+
+	Array.prototype.slice.call(document.getElementsByClassName('key')).map(function (element){
+		element.onclick=function(){ litero(element.getAttribute('data-letter'),ludo) };
+		element.style.visibility = "visible"
+	})
+	document.getElementById('selekto').onchange=niveloj
+	document.getElementById('mku').onclick=function(){malkaŝu(ludo)}
+	document.getElementById('nv').onclick=newgame
+	novaLudo(ludo)
+	return ludo
 }
+newgame()
+
+ 
